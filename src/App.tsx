e174c3e1a11046011dad08cd6f612106ab522a77
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2025-07-23 19:27:34 -0400

    feat: add AddTodoForm component and integrate it into App for adding todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,43 @@
 import { dummyData } from "./data/todos"
 import TodoItem from "./components/Todoitem"
+import { useState } from "react";
+import AddTodoForm from "./components/AddTodoForm";
 
 
 function App() {
+  const [ todos, setTodos] = useState(dummyData) 
 
   function setTodoCompleted(id: number, completed: boolean) {
-    alert(`Todo with id ${id} is now ${completed ? 'completed': 'notCompleted'}`)
+    setTodos((prevTodos) => 
+    prevTodos.map((todo) => 
+      (todo.id === id ? {...todo, completed}: todo))
+    )
   }
+
+  function addTodo(title:string){
+    setTodos((prevTodos) => [
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed: false,
+      },
+      ...prevTodos,
+    ])
+  }
+
+
+
   return (
     <main className="py-10 bg-blue-500 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">
         Your Todos
       </h1>
-      <div className='max-w-lg mx-auto bg-slate-100 rounded-md p-5'>
+      <div className='max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6'>
+        <AddTodoForm 
+          onSubmit={addTodo}
+        />
         <div className='space-y-2'>
-          { dummyData.map(todo => (
+          { todos.map(todo => (
             <TodoItem 
               key={todo.id}
               todo={todo}
