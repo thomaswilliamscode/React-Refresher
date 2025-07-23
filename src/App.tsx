c8d30d540d5854162d1ab5d98b4aefd0707c57c6
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2025-07-23 14:21:36 -0400

    add dummy data and implement todo list display in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { dummyData } from "./data/todos"
 
 
 function App() {
@@ -7,6 +8,15 @@ function App() {
       <h1 className="font-bold text-3xl text-center">
         Your Todos
       </h1>
+      <div className='max-w-lg mx-auto'>
+        <div className='space-y-2'>
+          { dummyData.map(todo => (
+            <p key={todo.id} className='text-lg'>
+              {todo.title}
+            </p>
+          ))}
+        </div>
+      </div>
     </main>
   )
 }
