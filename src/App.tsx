4c6a438c9afffd717d5c229f4daf12d68386f8f1
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2025-07-23 13:57:56 -0400

    fixed typescript intellisense not working due to tailwind update. project up and running properly.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@
 function App() {
 
   return (
-    <main>
-      <h1>
+    <main className="py-10 bg-blue-500 h-screen">
+      <h1 className="font-bold text-3xl text-center">
         Your Todos
       </h1>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
