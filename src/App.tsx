6455a982145246d37fd3bcaeb5dd1bafb3eaab22
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2025-07-24 15:00:48 -0400

    feat: implement TodoList and TodoSummary components, refactor App to utilize useTodos hook for state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { dummyData } from "./data/todos"
-import TodoItem from "./components/Todoitem"
-import { useState } from "react";
 import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+import TodoSummary from './components/TodoSummary';
+import useTodos from "./hooks/useTodos";
 
 
 function App() {
-  const [ todos, setTodos] = useState(dummyData) 
+  const {
+    todos,
+    setTodoCompleted,
+    addTodo,
+    deleteTodo,
+    deleteAllCompletedTodos
+  } = useTodos();
 
-  function setTodoCompleted(id: number, completed: boolean) {
-    setTodos((prevTodos) => 
-    prevTodos.map((todo) => 
-      (todo.id === id ? {...todo, completed}: todo))
-    )
-  }
-
-  function addTodo(title:string){
-    setTodos((prevTodos) => [
-      {
-        id: prevTodos.length + 1,
-        title,
-        completed: false,
-      },
-      ...prevTodos,
-    ])
-  }
-
-
-
+  
   return (
-    <main className="py-10 bg-blue-500 h-screen space-y-5">
+    <main className="py-10 bg-blue-500 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">
         Your Todos
       </h1>
@@ -36,16 +23,16 @@ function App() {
         <AddTodoForm 
           onSubmit={addTodo}
         />
-        <div className='space-y-2'>
-          { todos.map(todo => (
-            <TodoItem 
-              key={todo.id}
-              todo={todo}
-              onCompletedChange={setTodoCompleted}
-            />
-          ))}
-        </div>
+        <TodoList 
+          todos={todos}
+          onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo}
+        />
       </div>
+      <TodoSummary 
+        todos={todos}
+        deleteAllCompleted={deleteAllCompletedTodos}
+      />
     </main>
   )
 }
